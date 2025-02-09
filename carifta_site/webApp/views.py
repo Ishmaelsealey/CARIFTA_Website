deec8cff02a6258bf61d3ab5add5e0b4569345e5
Ishmael Sealey <ishmaelsealey@gmail.com>
2025-02-08 21:59:58 -0400

    added a template file and the homepage html file altered views.py to show the index page on the root url

--- carifta_site/webApp/views.py
+++ carifta_site/webApp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django import forms
 
 # Create your views here. each function is a different view/screen
 
-# index function is used to show a specific screen when it is called
+# index function is used to show a specific file when it is called
 def index(request):
-	return HttpResponse("Hello world!")
+	return render(request, 'website/index.html')
