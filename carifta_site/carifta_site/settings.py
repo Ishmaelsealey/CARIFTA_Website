d872fc2d37c3821fecaaed684722fc5717d611ed
Ishmael Sealey <ishysocial@gmail.com>
2025-04-05 21:19:37 -0400

    database settings updated for Postgres functionality

--- carifta_site/carifta_site/settings.py
+++ carifta_site/carifta_site/settings.py
@@ -86,14 +86,28 @@ WSGI_APPLICATION = 'carifta_site.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(
-		# default=os.environ.get('DATABASE_URL'),
-		default='sqlite:///db.sqlite3',
-		conn_max_age=600,
-		ssl_require=os.environ.get('RENDER', None) is not None  # Only require SSL on Render
-	)
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+# 		# default=os.environ.get('DATABASE_URL'),
+# 		default='sqlite:///db.sqlite3',
+# 		conn_max_age=600,
+# 		ssl_require=os.environ.get('RENDER', None) is not None  # Only require SSL on Render
+# 	)
+# }
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
